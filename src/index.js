6e118bb23781bb9a112d54a3e3c4897d0999aeca
Jose Familia <familiajoserene@gmail.com>
2023-11-13 19:12:17 -0400

    Add Docker and MongoDB configuration for Node.js app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+require('./database');
+
+app.use(require('./routes/index.routes'))
+
+app.listen(3000);
+console.log('Server on port', 3000);
+
+// Path: src/routes/index.js
+
